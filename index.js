d51ec25566cc8dfec45d1f312dc2120f30503f9f
Set Israel Ortiz Monasterio <pixrael777@gmail.com>
2020-11-18 21:56:36 -0300

    pointed to dist in order to serve the client

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/dist/my-poll-project-front`))
 
-app.get('/', (req, res) => {
-   res.send('Welcome to my server ');
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname));
 });
 
 app.listen(port, () => {
